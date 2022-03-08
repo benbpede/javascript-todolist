962bce688135ca7396a60fb9c54166f3b9da109d
benbpede <100111744+benbpede@users.noreply.github.com>
2022-03-08 18:28:33 +0900

    update background(darkmode)

--- js/background.js
+++ js/background.js
@@ -11,3 +11,21 @@ const bgImg = document.createElement("img");
 // document.body.appendChild(bgImg);
 
 document.body.style.background = `url(img/${randomImg}) `;
+
+//다크모드
+const body = document.querySelector("body");
+const white = document.querySelector("#whiteBtn");
+const black = document.querySelector("#blackBtn");
+
+black.addEventListener("click", darkMode);
+white.addEventListener("click", whiteMode);
+
+function darkMode() {
+  body.style = "";
+  body.classList.add("dark");
+}
+
+function whiteMode() {
+  document.body.style.background = `url(img/${randomImg}) `;
+  body.classList.remove("dark");
+}
